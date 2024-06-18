7a59d3cc42492a7a05cb8cd2b0bd3704570cbe67
Claude White <claude.white2005@gmail.com>
2024-06-18 10:36:55 -0400

    Initial Commit

---
+++ SummerProject/SummerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerProjectApp.swift
+//  SummerProject
+//
+//  Created by Claude White on 2024-06-18.
+//
+
+import SwiftUI
+
+@main
+struct SummerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
