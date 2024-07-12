344e73776680866a75007792c8eb97a9a67b9f8f
Claude White <claude.white2005@gmail.com>
2024-07-11 23:19:23 -0400

    Added rating progress circle and fixed fetch error by making belongsToCollection optional

---
+++ SummerProject/Views/Helpers/CircleProgress.swift
@@ -0,0 +1,18 @@
+//
+//  CircleProgress.swift
+//  SummerProject
+//
+//  Created by Claude White on 2024-07-11.
+//
+
+import SwiftUI
+
+struct CircleProgress: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleProgress()
+}
